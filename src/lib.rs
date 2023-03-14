afe32f5eb24b30f3f291a1b80724940778640f9c
Daniel R <nzdanron@gmail.com>
2023-03-14 19:28:34 +1300

    Got a demo of a character moving around the tic tac toe board
    Essentially this is gonna be how the player plays against the computer

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,14 @@
+use std::io::Write;
+
+use crossterm::{
+    cursor,
+    style::Print,
+    QueueableCommand, Result,
+};
+
 pub struct Grid {
     game_state: [CellState; 9],
+    previous_player_position: Option<usize>,
 }
 
 #[derive(Clone, Copy)]
@@ -7,37 +16,56 @@ enum CellState {
     X,
     O,
     Empty,
+    Cursor
 }
 
 impl Grid {
     pub fn new() -> Self {
         Self {
             game_state: [CellState::Empty; 9],
+            previous_player_position: None,
         }
     }
 
     /// Draws the game board
-    pub fn draw(&self) {
+    pub fn draw(&mut self, stdout: &mut std::io::Stdout, player_position: u8) -> Result<()> {
+        if let Some(index) = self. previous_player_position {
+            self.game_state[index] = CellState::Empty;
+        }
+
+        self.game_state[player_position as usize] = CellState::Cursor;
+        
+        self.previous_player_position = Some(player_position as usize);
+
         for y in 0..=2 {
-            let mut row = String::new();
+            let mut board = String::new();
             let offset = y * 3;
             for x in 0..=2 {
                 let char = match self.game_state[x + offset] {
                     CellState::X => 'X',
                     CellState::O => 'O',
                     CellState::Empty => ' ',
+                    CellState::Cursor => 'P',
                 };
 
                 match x {
-                    2 => row.push_str(&format!(" {} ", char)),
-                    _ => row.push_str(&format!(" {} │", char)),
+                    2 => board.push_str(&format!(" {} ", char)),
+                    _ => board.push_str(&format!(" {} │", char)),
                 }
             }
-            println!("{row}");
+            stdout.queue(Print(board))?;
+            stdout.queue(cursor::MoveDown(1))?;
+            stdout.queue(cursor::MoveToColumn(0))?;
+            stdout.flush()?;
 
             if y != 2 {
-                println!("───┼───┼───");
+                stdout.queue(Print("───┼───┼───"))?;
+                stdout.queue(cursor::MoveDown(1))?;
+                stdout.queue(cursor::MoveToColumn(0))?;
+                stdout.flush()?;
             }
         }
+
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,56 @@
+use std::io::{stdout, Write};
+
+use crossterm::{
+    cursor,
+    event::{read, Event, KeyCode},
+    terminal,
+    QueueableCommand, Result,
+};
+
 use tic_tac_toe::Grid;
 
-fn main() {
-    let game = Grid::new();
-    game.draw();
+fn main() -> Result<()> {
+    let mut stdout = stdout();
+    let mut player_position = 0;
+
+    stdout.queue(cursor::Hide)?;
+    terminal::enable_raw_mode()?;
+
+    let mut game = Grid::new();
+    loop {
+        game.draw(&mut stdout, player_position)?;
+
+        //stdout.queue(terminal::Clear(ClearType::FromCursorDown))?;
+
+        if let Event::Key(key) = read()? { match key.code {
+                KeyCode::Up => if player_position > 2 {
+                    player_position = player_position.saturating_sub(3)
+                },
+                KeyCode::Down => {
+                    if player_position < 6 {
+                        player_position += 3;
+                    }
+                },
+                KeyCode::Left => player_position = player_position.saturating_sub(1),
+                KeyCode::Right => {
+                    match player_position {
+                        2 | 5 | 8 => (),
+                        _ => player_position += 1
+                    };
+                },
+                KeyCode::Esc | KeyCode::Char('q') => break,
+                _ => (),
+            };
+        };
+
+        stdout.queue(cursor::MoveUp(5))?;
+        stdout.flush()?;
+    }
+
+    stdout.queue(cursor::Show)?;
+    terminal::disable_raw_mode()?;
+
+    Ok(())
+
+    //stdout.queue(cursor::MoveUp(5 + 1))?;
 }
