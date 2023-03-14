3997d378b978c976cd8bad3f29cdb7e2bb397c41
Daniel R <nzdanron@gmail.com>
2023-03-14 20:14:14 +1300

    Added collision on X's and O'x to the players movements
    Feeling a bit lazy so figured that instead of storing the prevoius
    positions state, the player just can't move there. This decision has
    cost me more than I'd like to admit as far as time goes to make
    possible, and I can already think of a case where this is screwed.
    
     x
    oPo
     x
    
    If P is the player cursor, then they are stuck, as they cannot jump over
    anywhere, yet that still needs to be a valid place to place a token. In
    this situation I'll need to design more code, perhaps moving down or up
    places you in the next avaliable square in that row? Doing that would
    also fix the following problem
    
    Pxo
    x
    x
    
    Here the player can also not escape

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ pub struct Game {
     previous_player_position: Option<usize>,
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, PartialEq, Eq)]
 enum CellState {
     X,
     O,
@@ -26,37 +26,76 @@ pub enum Direction {
 
 impl Game {
     pub fn new() -> Self {
+        let mut game_state = [CellState::Empty; 9];
+        game_state[3] = CellState::X;
+        game_state[1] = CellState::O;
+        game_state[7] = CellState::X;
         Self {
-            game_state: [CellState::Empty; 9],
+            game_state,
             previous_player_position: None,
             player_position: 0,
         }
     }
 
     pub fn move_player(&mut self, direction: Direction) {
+        let mut new_position = self.player_position;
         match direction {
             Direction::Up => {
                 if self.player_position > 2 {
-                    self.player_position = self.player_position.saturating_sub(3)
+                    new_position = new_position.saturating_sub(3)
+                }
+
+                while self.game_state[new_position as usize] != CellState::Empty {
+                    if new_position > 2 {
+                        new_position = new_position.saturating_sub(3)
+                    } else {
+                        break;
+                    }
                 }
             }
             Direction::Down => {
                 if self.player_position < 6 {
-                    self.player_position += 3;
+                    new_position += 3;
+                }
+                while self.game_state[new_position as usize] != CellState::Empty {
+                    if new_position < 6 {
+                        new_position += 3;
+                    } else {
+                        break;
+                    }
                 }
             }
-            Direction::Left => self.player_position = self.player_position.saturating_sub(1),
+            Direction::Left => {
+                new_position = new_position.saturating_sub(1);
+                while self.game_state[new_position as usize] != CellState::Empty {
+                    match new_position {
+                        0 | 3 | 6 => break,
+                        _ => new_position = new_position.saturating_sub(1),
+                    }
+                }
+            }
             Direction::Right => {
                 match self.player_position {
                     2 | 5 | 8 => (),
-                    _ => self.player_position += 1,
+                    _ => new_position += 1,
                 };
+
+                while self.game_state[new_position as usize] != CellState::Empty {
+                    match new_position {
+                        2 | 5 | 8 => break,
+                        _ => new_position += 1,
+                    };
+                }
             }
         };
+
+        if self.game_state[new_position as usize] == CellState::Empty {
+            self.player_position = new_position;
+        }
     }
 
     /// Draws the game board
-    pub fn draw(&mut self, stdout: &mut std::io::Stdout,) -> Result<()> {
+    pub fn draw(&mut self, stdout: &mut std::io::Stdout) -> Result<()> {
         if let Some(index) = self.previous_player_position {
             self.game_state[index] = CellState::Empty;
         }
